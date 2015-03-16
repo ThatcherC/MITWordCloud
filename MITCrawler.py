3b3ea8ea416cbf51d2c4b7b19a8cd9685a83dd31
ThatcherC <j.thatcher.c@gmail.com>
2015-03-16 19:06:33 -0400

    Can now access the full list of admitted students
    All 1000 or so of us!

--- MITCrawler.py
+++ MITCrawler.py
@@ -14,7 +14,7 @@ import locale
 import sys
 
 # Wrap sys.stdout into a StreamWriter to allow writing unicode.
-sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout) 
+#sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout) 
 #-----------------------------------------
 
 #set up BeautifulSoup and Mechanize
@@ -24,11 +24,22 @@ br = mechanize.Browser()
 br.set_handle_robots(False)
 br.set_cookiejar(cj)
 
+#sign in to MyMIT
 br.open("https://my.mit.edu/uaweb/login.htm")
 br.select_form(nr=0)
 br.form['j_username'] = settings.creds['username']
 br.form['j_password'] = settings.creds['password']
 br.submit()
 
-#sign in to MyMIT
-print br.response().read()
+br.open('https://my.mit.edu/uaweb/guestbook.htm?_flowId=guestbook-flow')
+
+#soup = BeautifulSoup(br.response().read())
+
+br.find_link(text='Show All')
+req = br.click_link(text='Show All')
+br.open(req)
+
+
+
+#nameLinks = soup.findAll('a')
+#print nameLinks[31]
